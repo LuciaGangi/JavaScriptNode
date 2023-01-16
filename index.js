6744c32bdbfc2e69278962e7a52c0d8eb75f928f
unknown <lucia.gangi@mi.unc.edu.ar>
2023-01-16 17:09:10 -0300

    ejercicio2

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const miInformacion = {
+    nombre: "Lucía",
+    edad: 28,
+    desarrollador: true,
+    fechaNacimiento: new Date(),
+    libroFavorito: {
+        titulo: "El hombre en busca de sentido",
+        autor: "Viktor Frankl",
+        fecha: "1946",
+        url: "https://es.wikipedia.org/wiki/El_hombre_en_busca_de_sentido"
+    }
+};
 
 
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
